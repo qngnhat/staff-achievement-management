698e0c46bb1aae178c9e7f1c1803a9aaef1e0b36
qngnhat <qngnhat@gmail.com>
2021-03-29 23:42:54 +0700

    them giao dien home

--- src/main/java/com/nez/StaffAchievementManagement/controller/AppController.java
+++ src/main/java/com/nez/StaffAchievementManagement/controller/AppController.java
@@ -1,18 +1,9 @@
 package com.nez.StaffAchievementManagement.controller;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.nez.StaffAchievementManagement.model.User;
-import com.nez.StaffAchievementManagement.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class AppController {
